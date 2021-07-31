a700bf36577e430598263d37b1cc4c35e13ea007
harika <e.harika78@gmail.com>
2021-08-01 00:32:33 +0530

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,8 +3,13 @@ import React,{Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component{
-
+  handleClick =(buttonText)=>{
+    if(buttonText===this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler()
+    }
+  }
     render(){
+      const answers = this.props.quiz_question.answer_options
         return(
         <main>
         <section>
@@ -12,7 +17,11 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((ans,index)=>{
+              return <QuizQuestionButton key={index} button_text={ans} 
+               clickHandler={this.handleClick.bind(this)}/>
+            })}
+              
       
           </ul>
         </section>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,10 +2,14 @@ import React,{Component} from 'react'
 
 
 class QuizQuestionButton extends Component{
+    handleClick =(event)=>{
+        console.log("quizbutton",event.target.value)
+        this.props.clickHandler(event.target.value)
+    }
     render(){
         return(
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={this.handleClick.bind(this)} value={this.props.button_text}>{this.props.button_text}</button>
             </li>
         )
     }
